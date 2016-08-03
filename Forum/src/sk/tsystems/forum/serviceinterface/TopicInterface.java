9e7f48b8997a3a6550690f11d6dfc7f183ca7c7d
DaliborAdamcik <diaceu@gmail.com>
2016-08-03 11:42:50 +0200

    Interface - all services

--- Forum/src/sk/tsystems/forum/serviceinterface/TopicInterface.java
+++ Forum/src/sk/tsystems/forum/serviceinterface/TopicInterface.java
@@ -1,5 +1,17 @@
 package sk.tsystems.forum.serviceinterface;
 
-public interface TopicInterface {
+import java.util.List;
 
+import sk.tsystems.forum.entity.Topic;
+
+public interface TopicInterface {
+	boolean addTopic(Topic topic);
+
+	boolean removeTopic(Topic topic);
+
+	boolean updateTopic(Topic topic);
+
+	Topic getTopic(int ident);
+
+	List<Topic> getTopics();
 }
