033f9ffe6cba9dd1a01e80374413fb10c4e56922
Dalibor <Dalibor@dalibor-pc>
2016-08-07 22:13:09 +0200

    Register - prepare

--- Forum/WebContent/js/register.js
+++ Forum/WebContent/js/register.js
@@ -1,3 +1,36 @@
+//http://stackoverflow.com/questions/3831680/httpservletrequest-get-json-post-data
+//$('form [name="reg"]');
+$('#reg').submit(function(ev){
+	
+    var $form = $('#reg');
+    console.log($form);
+    console.log($form.serialize());
+    console.log($form.serializeObject());
+    console.log(JSON.stringify($form.serializeObject()));
+    console.log(JSON.stringify($form.serialize()));
+    console.log(JSON.stringify($form));
+	
+    
+    return false; // prevent subnit form
+});
+
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
+
 function checkPass() {
 	var pass1 = document.getElementById('reg_pass1');
 	var pass2 = document.getElementById('reg_pass2');
@@ -15,3 +48,4 @@ function checkPass() {
 		message.innerHTML = "Passwords Do Not Match!"
 	}
 }
+
