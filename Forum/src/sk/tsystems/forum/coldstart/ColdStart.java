e92b89ea1353372616c3364b83956292884cafe4
Jozef.Ulicny <ulicny.j.r.h@gmail.com>
2016-08-09 13:18:28 +0200

    coldStart update

--- Forum/src/sk/tsystems/forum/coldstart/ColdStart.java
+++ Forum/src/sk/tsystems/forum/coldstart/ColdStart.java
@@ -2,6 +2,8 @@ package sk.tsystems.forum.coldstart;
 
 import java.util.Date;
 
+
+import sk.tsystems.forum.entity.Blocked;
 import sk.tsystems.forum.entity.Comment;
 import sk.tsystems.forum.entity.Topic;
 import sk.tsystems.forum.entity.User;
@@ -17,9 +19,10 @@ import sk.tsystems.forum.serviceinterface.UserInterface;
  *
  */
 public class ColdStart {
-	private UserInterface userColdStart;
+	private UserInterface userService;
 	private CommentInterface commentColdStart;
 	private TopicInterface topicColdStart;
+	private Blocked blocked;
 	
 	User janka;
 	User jano;
@@ -29,7 +32,7 @@ public class ColdStart {
 	
 	
 	public ColdStart() {
-		this.userColdStart = new UserJPA();
+		this.userService = new UserJPA();
 		this.topicColdStart = new TopicJPA();
 		this.commentColdStart = new CommentJPA();
 	}
@@ -42,23 +45,60 @@ public class ColdStart {
 		User dalibor = new User("Dalik", "123456", new Date(), "Dalibor");
 		User tomas = new User("Tomas", "123456", new Date(), "Tomas");
 		
+		
 		// user service persist users 
-
+		userService.addUser(janka);
+		userService.addUser(jano);
+		userService.addUser(jozo);
+		userService.addUser(dalibor);
+		userService.addUser(tomas);
 		
 		// volat inicialiyacie pre userov
 		jankaInicialize(janka);
+		janoInicialize(jano);
+		jozoInicialize(jozo);
 		
-		userColdStart.addUser(new User("Janka", "123456", new Date(), "Jana"));
+		userService.addUser(new User("Janka", "123456", new Date(), "Jana"));
 	}
 	
 	public void jankaInicialize(User user){
-		Topic topic =new Topic("Lietadla", true);
+		
+		Topic topic = new Topic("Lietadla", true);
 		topicColdStart.addTopic(topic);
+		
 		commentColdStart.addComment(new Comment("Sehr schon", topic, user, true));
 		// persistnut
 	}
 	
+	public void janoInicialize(User user){
+		Topic topic = new Topic("Auta", false);
+		topicColdStart.addTopic(topic);
+		commentColdStart.addComment(new Comment("Paraaada", topic, user, false));
+	}
+	
+	public void jozoInicialize(User user){
+		jozo.setBlocked(blocked);
+		Topic topic = new Topic("Motorky", false);
+		topicColdStart.addTopic(topic);
+		commentColdStart.addComment(new Comment("zuum zuuumm", topic, user, false));
+		
+	}
+	
+	public void daliborInicialize(User user){
+		Topic topic = new Topic("Vlaky", true);
+		topicColdStart.addTopic(topic);
+		commentColdStart.addComment(new Comment("Dobree", topic, user, true));
+		
+	}
+	
+	public void tomasInicialize(User user){
+		Topic topic = new Topic("Tazne stroje", true);
+		topicColdStart.addTopic(topic);
+		commentColdStart.addComment(new Comment("Paradne", topic, user, true));
+		
+	}
+	
+	
 }
 
 
-//topic, 
