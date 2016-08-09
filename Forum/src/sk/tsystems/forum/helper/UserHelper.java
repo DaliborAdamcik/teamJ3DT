345c2db36b0db7f4bd4ca97acd185c5a3d478c2f
JanaKrajnakova <krajnakova.jana20@gmail.com>
2016-08-09 14:33:02 +0200

    UserHelper

--- Forum/src/sk/tsystems/forum/helper/UserHelper.java
+++ Forum/src/sk/tsystems/forum/helper/UserHelper.java
@@ -8,94 +8,76 @@ public class UserHelper {
 
 	/***
 	 * password validator part
+	 * 
 	 * @author Janka
 	 */
-	public boolean passwordOverallControll(String password) throws PasswordCheckException {
-		if(password == null)
+	public static void passwordOverallControll(String password) throws PasswordCheckException {
+		if (password == null)
 			throw new PasswordCheckException("Password cannot be null");
-			
-		if (!charactersControll(password)) 
-			throw new PasswordCheckException("your password consists invalid character/s, choose only from A-Za-z0-9!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~");
-		
-		if (!lengthControll(password)) 
-			throw new PasswordCheckException("your password must have at least 8 characters");
-
-		if (!digitControll(password)) 
-			throw new PasswordCheckException("your password have to contain at least 1 digit");
-
-		if (!specialCharacterControll(password)) 
-				throw new PasswordCheckException("your password have to contain at least 1 special character");
-
-		return true;
+		charactersControll(password);
+		lengthControll(password);
+		digitControll(password);
+		specialCharacterControll(password);
 	}
 
-	private boolean charactersControll(String password) {
-		// Pattern pattern =
-		// Pattern.compile("^[A-Za-z0-9!\"#$%&'()*+,-./:;<=>?@[\\]\\^\\_\\`{|}~]{1,}$");
+	private static void charactersControll(String password) throws PasswordCheckException {
 		Pattern pattern = Pattern.compile("^[!-~]{1,}$");
 		Matcher matcher = pattern.matcher(password);
-		if (matcher.matches()) {
-			return true;
+		if (!matcher.matches()) {
+			throw new PasswordCheckException(
+					"your password consists invalid character/s, choose only from A-Za-z0-9!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~");
 		}
-		return false;
 	}
 
-	private boolean lengthControll(String password) {
-		// System.out.println("legtnh " + password.trim().length());
-		if (password.trim().length() > 7) {
-			return true;
-		}
-		return false;
+	private static void lengthControll(String password) throws PasswordCheckException {
+		if (password.length() < 8)
+			throw new PasswordCheckException("your password must have at least 8 characters");
 	}
 
-	private boolean digitControll(String password) {
+	private static void digitControll(String password) throws PasswordCheckException {
 		for (int i = 0; i < password.length(); i++) {
 			if (Character.isDigit(password.charAt(i))) {
-				// System.out.println(password.charAt(i));
-				return true;
+				return;
 			}
 		}
-		return false;
+		throw new PasswordCheckException("your password have to contain at least 1 digit");
 	}
 
-	private boolean specialCharacterControll(String password) {
+	private static void specialCharacterControll(String password) throws PasswordCheckException {
 		for (int i = 0; i < password.length(); i++) {
 			int ascii = (int) password.charAt(i);
-			// System.out.println((int) password.charAt(i));
 			if ((ascii > 32 && ascii < 48) || (ascii > 57 && ascii < 65) || (ascii > 90 && ascii < 97)
 					|| (ascii > 122 && ascii < 127)) {
-				return true;
+				return;
 			}
 		}
-		return false;
+		throw new PasswordCheckException("your password have to contain at least 1 special character");
 	}
 
 	public static void main(String[] args) {
-		UserHelper ps = new UserHelper();
 		try {
-			ps.passwordOverallControll(null);
-			ps.passwordOverallControll("p!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~as^swo\\rd//<5");
-			ps.passwordOverallControll("p!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~as^swo\\rd//<5");
+			//passwordOverallControll("pas");
+			passwordOverallControll("12345678");
 			System.out.println("ok");
 		} catch (PasswordCheckException e) {
 			System.out.println(e.getMessage());
-			//e.printStackTrace();
+			// e.printStackTrace();
 		}
 	}
-	
+
 	/***
 	 * nick name validator PART
+	 * 
 	 * @author Dalibor
 	 */
-	public static boolean nickNameValidator(String userName) throws NickNameException
-	{
-    	if(userName.length()<4)
-    		throw new NickNameException("Nickname must be 4 characters long");
-    	
-    	Pattern testValidNickName = Pattern.compile("^([a-z][a-z0-9]{3,})$");
-    	if(!testValidNickName.matcher(userName).matches())
-    		throw new NickNameException("Nickname can contain a-z and 0-9 characters. First character muste be a-z.");
-    	
-    	return true;
+	public static boolean nickNameValidator(String userName) throws NickNameException {
+		if (userName.length() < 4)
+			throw new NickNameException("Nickname must be 4 characters long");
+
+		Pattern testValidNickName = Pattern.compile("^([a-z][a-z0-9]{3,})$");
+		if (!testValidNickName.matcher(userName).matches())
+			throw new NickNameException("Nickname can contain a-z and 0-9 characters. First character must be a-z.");
+
+		return true;
 	}
 }
