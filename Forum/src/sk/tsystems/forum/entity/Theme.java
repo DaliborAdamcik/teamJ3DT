e08ceb0edc5c5f36ab967562a5b2de02420b4a69
JanaKrajnakova <krajnakova.jana20@gmail.com>
2016-08-11 15:32:14 +0200

    Exchange Theme<->Topic

--- Forum/src/sk/tsystems/forum/entity/Theme.java
+++ Forum/src/sk/tsystems/forum/entity/Theme.java
@@ -1,11 +1,10 @@
 package sk.tsystems.forum.entity;
 
 import java.util.Date;
-import java.util.List;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import sk.tsystems.forum.entity.common.BlockableEntity;
@@ -15,48 +14,65 @@ import sk.tsystems.forum.entity.common.BlockableEntity;
 public class Theme  extends BlockableEntity {
 
 	/**
-	 * Name of the theme
-	 * 
+	 * name of the topic
 	 */
-	@Column(name = "NAME", nullable = false)
+	@Column(name = "NAME", nullable = false, unique = true)
 	private String name;
 
 	/**
-	 * Public status for theme
+	 * theme
+	 */
+	@ManyToOne
+	@JoinColumn(name = "THEMEID")
+	private Theme theme;
+
+	/**
+	 * description of topic
+	 */
+	@Column(name = "DESCRIPTION", nullable = false)
+	private String description;
+
+	/**
+	 * rating of topic (default = 0) modified by up / down votes
+	 */
+	@Column(name = "RATING", nullable = false)
+	private int rating;
+
+	/**
+	 * author
+	 */
+	//@Column(name = "AUTHOR", nullable = false)
+	@ManyToOne
+	private User author;
+
+	/**
+	 * Public status for topic
 	 */
 	@Column(name = "ISPUBLIC", nullable = false)
 	private boolean isPublic;
+	// TODO implementovat list kommentov ak nam ho bude treba
+	// @OneToMany
+	// List<Comment> comments;
 
-	/**
-	 * List of topics
-	 */
-	@OneToMany
-	private List<Topic> topics;
-
-	/**
-	 * Theme constructor
-	 * 
-	 * @param name
-	 * @param isPublic
-	 */
-	public Theme(String name, boolean isPublic) {
+	public Theme(String name, Theme theme, String description, int rating, User author, boolean isPublic) {
 		this();
 		setName(name);
+		setTheme(theme);
+		setDescription(description);
+		setRating(rating);
+		setAuthor(author);
 		setPublic(isPublic);
 	}
-	
+
 	/**
-	 * Non-Parametric constructor used by HIBERNATE
-	 * (INFO: HHH000182: No default (no-argument) constructor for class: 
-	 * 	sk.tsystems.forum.entity.Theme (class must be instantiated by Interceptor))
+	 * Constructor for JPA
 	 */
-	private Theme()
-	{
+	private Theme() {
 		super();
 	}
 
 	/**
-	 * Getter for theme name
+	 * Getter for name
 	 * 
 	 * @return name
 	 */
@@ -71,6 +87,92 @@ public class Theme  extends BlockableEntity {
 	 */
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	/**
+	 * Getter for theme
+	 * 
+	 * @return theme
+	 */
+	public Theme getTheme() {
+		return theme;
+	}
+
+	/**
+	 * Setter for theme
+	 * 
+	 * @param theme
+	 */
+	public void setTheme(Theme theme) {
+		this.theme = theme;
+	}
+
+	/**
+	 * Getter for description
+	 * 
+	 * @return description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * Setter for description
+	 * 
+	 * @param description
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * Getter for rating
+	 * 
+	 * @return rating
+	 */
+	public int getRating() {
+		return rating;
+	}
+
+	/**
+	 * Setter for rating
+	 * 
+	 * @param rating
+	 */
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	/**
+	 * Rating increase
+	 */
+	public void ratingIncrease() {
+		rating++;
+	}
+
+	/**
+	 * Rating decrease
+	 */
+	public void ratingDecrease() {
+		rating--;
+	}
+
+	/**
+	 * Getter for author
+	 * 
+	 * @return author
+	 */
+	public User getAuthor() {
+		return author;
+	}
+
+	/**
+	 * Setter for author
+	 * 
+	 * @param author
+	 */
+	public void setAuthor(User author) {
+		this.author = author;
 	}
 
 	/**
@@ -93,21 +195,17 @@ public class Theme  extends BlockableEntity {
 
 	/**
 	 * Getter for creationDate
-	 * <p>This method returns date of Theme create</p>
-	 * <p><b><i>DEPRECATED</i></b> Please use getCreated() instead.</p>
+	 * <p>
+	 * This method returns date of topic create
+	 * </p>
+	 * <p>
+	 * <b><i>DEPRECATED</i></b> Please use getCreated() instead.
+	 * </p>
+	 * 
 	 * @return creationDate (return value is same as getCreated)
 	 */
 	@Deprecated
 	public Date getCreationDate() {
 		return getCreated();
-	}
-
-	/**
-	 * Getter for topics
-	 * 
-	 * @return topics
-	 */
-	public List<Topic> getTopics() {
-		return topics;
 	}
 }
--- Forum/src/sk/tsystems/forum/entity/Topic.java
+++ Forum/src/sk/tsystems/forum/entity/Topic.java
@@ -1,10 +1,11 @@
 package sk.tsystems.forum.entity;
 
 import java.util.Date;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import sk.tsystems.forum.entity.common.BlockableEntity;
@@ -14,65 +15,48 @@ import sk.tsystems.forum.entity.common.BlockableEntity;
 public class Topic extends BlockableEntity {
 
 	/**
-	 * name of the topic
+	 * Name of the theme
+	 * 
 	 */
-	@Column(name = "NAME", nullable = false, unique = true)
+	@Column(name = "NAME", nullable = false)
 	private String name;
 
 	/**
-	 * theme
-	 */
-	@ManyToOne
-	@JoinColumn(name = "THEMEID")
-	private Theme theme;
-
-	/**
-	 * description of topic
-	 */
-	@Column(name = "DESCRIPTION", nullable = false)
-	private String description;
-
-	/**
-	 * rating of topic (default = 0) modified by up / down votes
-	 */
-	@Column(name = "RATING", nullable = false)
-	private int rating;
-
-	/**
-	 * author
-	 */
-	//@Column(name = "AUTHOR", nullable = false)
-	@ManyToOne
-	private User author;
-
-	/**
-	 * Public status for topic
+	 * Public status for theme
 	 */
 	@Column(name = "ISPUBLIC", nullable = false)
 	private boolean isPublic;
-	// TODO implementovat list kommentov ak nam ho bude treba
-	// @OneToMany
-	// List<Comment> comments;
-
-	public Topic(String name, Theme theme, String description, int rating, User author, boolean isPublic) {
-		this();
-		setName(name);
-		setTheme(theme);
-		setDescription(description);
-		setRating(rating);
-		setAuthor(author);
-		setPublic(isPublic);
-	}
 
 	/**
-	 * Constructor for JPA
+	 * List of topics
 	 */
-	private Topic() {
+	@OneToMany
+	private List<Topic> topics;
+
+	/**
+	 * Theme constructor
+	 * 
+	 * @param name
+	 * @param isPublic
+	 */
+	public Topic(String name, boolean isPublic) {
+		this();
+		setName(name);
+		setPublic(isPublic);
+	}
+	
+	/**
+	 * Non-Parametric constructor used by HIBERNATE
+	 * (INFO: HHH000182: No default (no-argument) constructor for class: 
+	 * 	sk.tsystems.forum.entity.Theme (class must be instantiated by Interceptor))
+	 */
+	private Topic()
+	{
 		super();
 	}
 
 	/**
-	 * Getter for name
+	 * Getter for theme name
 	 * 
 	 * @return name
 	 */
@@ -87,92 +71,6 @@ public class Topic extends BlockableEntity {
 	 */
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	/**
-	 * Getter for theme
-	 * 
-	 * @return theme
-	 */
-	public Theme getTheme() {
-		return theme;
-	}
-
-	/**
-	 * Setter for theme
-	 * 
-	 * @param theme
-	 */
-	public void setTheme(Theme theme) {
-		this.theme = theme;
-	}
-
-	/**
-	 * Getter for description
-	 * 
-	 * @return description
-	 */
-	public String getDescription() {
-		return description;
-	}
-
-	/**
-	 * Setter for description
-	 * 
-	 * @param description
-	 */
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	/**
-	 * Getter for rating
-	 * 
-	 * @return rating
-	 */
-	public int getRating() {
-		return rating;
-	}
-
-	/**
-	 * Setter for rating
-	 * 
-	 * @param rating
-	 */
-	public void setRating(int rating) {
-		this.rating = rating;
-	}
-
-	/**
-	 * Rating increase
-	 */
-	public void ratingIncrease() {
-		rating++;
-	}
-
-	/**
-	 * Rating decrease
-	 */
-	public void ratingDecrease() {
-		rating--;
-	}
-
-	/**
-	 * Getter for author
-	 * 
-	 * @return author
-	 */
-	public User getAuthor() {
-		return author;
-	}
-
-	/**
-	 * Setter for author
-	 * 
-	 * @param author
-	 */
-	public void setAuthor(User author) {
-		this.author = author;
 	}
 
 	/**
@@ -195,17 +93,21 @@ public class Topic extends BlockableEntity {
 
 	/**
 	 * Getter for creationDate
-	 * <p>
-	 * This method returns date of topic create
-	 * </p>
-	 * <p>
-	 * <b><i>DEPRECATED</i></b> Please use getCreated() instead.
-	 * </p>
-	 * 
+	 * <p>This method returns date of Theme create</p>
+	 * <p><b><i>DEPRECATED</i></b> Please use getCreated() instead.</p>
 	 * @return creationDate (return value is same as getCreated)
 	 */
 	@Deprecated
 	public Date getCreationDate() {
 		return getCreated();
+	}
+
+	/**
+	 * Getter for topics
+	 * 
+	 * @return topics
+	 */
+	public List<Topic> getTopics() {
+		return topics;
 	}
 }
