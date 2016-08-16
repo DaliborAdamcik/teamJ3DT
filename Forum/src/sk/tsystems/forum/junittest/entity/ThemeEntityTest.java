3b5a0f5e4a063397abfb0d35d7ff946b27e12781
amaurote <acey.music@gmail.com>
2016-08-16 10:50:18 +0200

    theme entity test

--- Forum/src/sk/tsystems/forum/junittest/entity/ThemeEntityTest.java
+++ Forum/src/sk/tsystems/forum/junittest/entity/ThemeEntityTest.java
@@ -8,24 +8,36 @@ import org.junit.Before;
 import org.junit.Test;
 
 import sk.tsystems.forum.entity.Theme;
+import sk.tsystems.forum.entity.Topic;
+import sk.tsystems.forum.entity.User;
 import sk.tsystems.forum.helper.TestHelper;
 
 public class ThemeEntityTest {
 	
 	
 	private String name;
+	private String pass;
+	private String description;
 	private boolean isPublic;
+	private Topic topic;
+	private User author;
 	
 	@Before
 	public void setUp() throws Exception {
-		name = TestHelper.randomString(20);
-		// creationDate = new Date();
+		name = TestHelper.randomString(20,0).toLowerCase();
+		//pass = TestHelper.randomString(10,10) + ".";
+		pass = "AbcdEfgh.123456789";
+		description = TestHelper.randomString(40);
+		
+		topic = new Topic(name, isPublic);
+		author = new User(name, pass, TestHelper.randomDate(), name);
+		
 		isPublic = false;
 	}
 
 	@Test
 	public void getNameTest() {
-		Theme randomTheme = new Theme(name, null, name, null, isPublic);
+		Theme randomTheme = new Theme(name, topic, description, author, isPublic);
 		String testName = randomTheme.getName();
 
 		assertEquals("Bad name", name, testName);
@@ -33,7 +45,7 @@ public class ThemeEntityTest {
 	
 	@Test
 	public void setNameTest() {
-		Theme randomTheme = new Theme(TestHelper.randomString(20), null, name, null, isPublic);
+		Theme randomTheme = new Theme(name, topic, description, author, isPublic);
 		randomTheme.setName(name);
 		String testName = randomTheme.getName();
 
@@ -42,7 +54,7 @@ public class ThemeEntityTest {
 	
 	@Test
 	public void isPublic() {
-		Theme randomTheme = new Theme(name, null, name, null, isPublic);
+		Theme randomTheme = new Theme(name, topic, description, author, isPublic);
 		boolean testIsPublic = randomTheme.isIsPublic();
 
 		assertEquals("Bad isPublic", isPublic, testIsPublic);
@@ -50,7 +62,7 @@ public class ThemeEntityTest {
 	
 	@Test
 	public void setPublic() {
-		Theme randomTheme = new Theme(name, null, name, null, true);
+		Theme randomTheme = new Theme(name, topic, description, author, isPublic);
 		randomTheme.setPublic(isPublic);
 		boolean testIsPublic = randomTheme.isIsPublic();
 
@@ -60,8 +72,8 @@ public class ThemeEntityTest {
 	@Test
 	public void getCreationDate() {
 		Date creationDate = new Date();
-		Theme randomTheme = new Theme(name, null, name, null, isPublic);
-		assertEquals("Bad creation date", creationDate, randomTheme.getCreated());
+		Theme randomTheme = new Theme(name, topic, description, author, isPublic);
+		assertEquals("Bad creation date", creationDate.getTime()/100, randomTheme.getCreated().getTime()/100);
 	}
 
 }
