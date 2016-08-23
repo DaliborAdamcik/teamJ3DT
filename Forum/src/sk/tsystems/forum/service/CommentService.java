b14dea8b47168c1cb2c6b04d8573a50486238ed4
JanaKrajnakova <krajnakova.jana20@gmail.com>
2016-08-23 15:30:36 +0200

    comment service doc

--- Forum/src/sk/tsystems/forum/service/CommentService.java
+++ Forum/src/sk/tsystems/forum/service/CommentService.java
@@ -13,21 +13,21 @@ public interface CommentService {
 	 * Adds comment to the database
 	 * 
 	 * @param comment
-	 * @return true if successful, false othervise
+	 * @return true if successful, false otherwise
 	 */
 	boolean addComment(Comment comment);
 	/**
 	 * Removes comment from the database
 	 * 
 	 * @param comment
-	 * @return true if successful, false othervise
+	 * @return true if successful, false otherwise
 	 */
 	boolean removeComment(Comment comment);
 	/**
 	 * Update comment in the database
 	 * 
 	 * @param comment
-	 * @return true if successful, false othervise
+	 * @return true if successful, false otherwise
 	 */
 	boolean updateComment(Comment comment);
 
@@ -60,15 +60,34 @@ public interface CommentService {
 	/**
 	 * Returns all comments that were created by certain user
 	 * 
-	 * @param owner (user that created comment)
+	 * @param owner (user who created comment)
 	 * @return list of comments written by certain user
 	 */
 	List<Comment> getComments(User owner);
 	
 	
-	//TODO dorobit doc
+	/**
+	 * Rating of certain comment from certain owner
+	 * 
+	 * @param owner (user who rated the comment)
+	 * @param comment which was rated
+	 * @return rating of comment and owner from parameter
+	 */
 	CommentRating getCommentRating(User owner, Comment comment) ;
+	
+	/**
+	 * Returns all comments from the database
+	 * 
+	 * @return list of comments from the database
+	 */
 	List<Comment> getAllComments();
+	
+	/**
+	 * All ratings that were created by certain user
+	 * 
+	 * @param owner (user who rated the comment)
+	 * @return list of ratings from specified user
+	 */
 	List<CommentRating> getAllCommentRatings(User owner);
 	
 	
