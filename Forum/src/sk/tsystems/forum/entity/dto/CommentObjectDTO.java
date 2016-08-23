001d5791db5aabdd4a545020b4606a90dc080db1
JanaKrajnakova <krajnakova.jana20@gmail.com>
2016-08-23 11:07:05 +0200

    javadoc DTO

--- Forum/src/sk/tsystems/forum/entity/dto/CommentObjectDTO.java
+++ Forum/src/sk/tsystems/forum/entity/dto/CommentObjectDTO.java
@@ -4,36 +4,73 @@ import sk.tsystems.forum.entity.Comment;
 import sk.tsystems.forum.service.jpa.JpaConnector;
 
 public class CommentObjectDTO {
+
+	/** field <b>count of ratings for comment</b> */
 	private long count;
+
+	/** field <b>rating for comment</b> */
 	private long rating;
-	
+
+	/**
+	 * Constructor for CommentObjectDTO
+	 * 
+	 * @param count
+	 *            of ratings for comment
+	 * @param rating
+	 *            for comment
+	 */
 	public CommentObjectDTO(long count, long rating) {
 		super();
 		this.count = count;
 		this.rating = rating;
 	}
 
+	/**
+	 * returns CommentObjectDTO from database
+	 * 
+	 * @param comment
+	 *            {@link Comment}
+	 * @return CommentObjectDTO for specified comment from parameter
+	 */
 	public static CommentObjectDTO getDTO(Comment comment) {
 		try (JpaConnector jpa = new JpaConnector()) {
-			return jpa.getEntityManager().createQuery("SELECT NEW sk.tsystems.forum.entity.dto.CommentObjectDTO(count(c), sum(c.rating)) FROM CommentRating c "
-					+ "WHERE c.comment = :comment GROUP BY c.comment", CommentObjectDTO.class).setParameter("comment", comment).getSingleResult();
-		}
-		catch(javax.persistence.NoResultException e) {
+			return jpa.getEntityManager()
+					.createQuery(
+							"SELECT NEW sk.tsystems.forum.entity.dto.CommentObjectDTO(count(c), sum(c.rating)) FROM CommentRating c "
+									+ "WHERE c.comment = :comment GROUP BY c.comment",
+							CommentObjectDTO.class)
+					.setParameter("comment", comment).getSingleResult();
+		} catch (javax.persistence.NoResultException e) {
 			return new CommentObjectDTO(0, 0);
 		}
 	}
 
+	/**
+	 * Getter for count
+	 * 
+	 * @return long count of ratings for comment
+	 */
 	public long getCount() {
 		return count;
 	}
 
+	/**
+	 * Getter for rating
+	 * 
+	 * @return long rating for comment
+	 */
 	public long getRating() {
 		return rating;
 	}
 
+	/**
+	 * String representation of this DTO object
+	 * 
+	 * @return {@link String} representation of CommentObjectDTO
+	 */
 	@Override
 	public String toString() {
 		return "CommentObjectDTO [count=" + count + ", rating=" + rating + "]";
 	}
-	
+
 }
--- Forum/src/sk/tsystems/forum/entity/dto/ThemeObjectDTO.java
+++ Forum/src/sk/tsystems/forum/entity/dto/ThemeObjectDTO.java
@@ -7,13 +7,34 @@ import sk.tsystems.forum.service.jpa.JpaConnector;
 
 public class ThemeObjectDTO {
 
+	/** field <b>rating for theme</b> */
 	private long sumRating;
+	
+	/** field <b>count of ratings for theme</b> */
 	private long ratingCount;
+	
+	/** field <b>count of comments for theme</b> */
 	private long commentCount;
+	
+	/** field <b>count of users for theme</b> */
 	private long userCount;
+	
+	/** field <b>last's comment date {@link Date}</b> */
 	private Date lastCommentDate;
+	
+	/** field <b>{@link Theme}</b> */
 	private Theme theme;
 
+	/**
+	 * Constructor for ThemeObjectDTO
+	 * 
+	 * @param theme {@link Theme}
+	 * @param averageRating rating for theme
+	 * @param ratingCount count of ratings for theme
+	 * @param commentCount count of comments for theme
+	 * @param userCount count of users for theme
+	 * @param lastCommentDate {@link Date} last's comment date 
+	 */
 	public ThemeObjectDTO(Theme theme, long averageRating, long ratingCount, long commentCount, long userCount, Date lastCommentDate) {
 		super();
 		this.sumRating = averageRating;
@@ -24,15 +45,35 @@ public class ThemeObjectDTO {
 		this.theme = theme;
 	}
 
+	/**
+	 * Constructor for ThemeObjectDTO
+	 * 
+	 * @param theme {@link Theme}
+	 * @param commentCount count of comments for theme
+	 * @param userCount count of users for theme
+	 * @param lastCommentDate {@link Date} last's comment date 
+	 */
 	public ThemeObjectDTO(Theme theme, long commentCount, long userCount, Date lastCommentDate) {
 		this(theme, 0,0, commentCount, userCount, lastCommentDate);
 	}
 
+	/**
+	 * Constructor for ThemeObjectDTO
+	 * 
+	 * @param theme {@link Theme}
+	 * @param averageRating rating for theme
+	 * @param ratingCount count of ratings for theme 
+	 */
 	public ThemeObjectDTO(Theme theme, long averageRating, long ratingCount) {
 		this(theme, averageRating, ratingCount, 0, 0, null);
 	}
 	
-	
+	/**
+	 * return ThemeObjectDTO from database
+	 * 
+	 * @param theme {@link Theme}
+	 * @return ThemeObjectDTO for specified theme from parameter
+	 */
 	public static ThemeObjectDTO getDTO(Theme theme) {
 		try (JpaConnector jpa = new JpaConnector()) {
 			ThemeObjectDTO obj1;
@@ -65,26 +106,56 @@ public class ThemeObjectDTO {
 		}
 	}
 
+	/**
+	 * Getter for average rating
+	 * 
+	 * @return long sumRating for theme
+	 */
 	public long getAverageRating() {
 		return sumRating;
 	}
 
+	/**
+	 * Getter for rating count
+	 * 
+	 * @return long ratingCount for theme
+	 */
 	public long getRatingCount() {
 		return ratingCount;
 	}
 
+	/**
+	 * Getter for comment count
+	 * 
+	 * @return long commentCount for theme
+	 */
 	public long getCommentCount() {
 		return commentCount;
 	}
 
+	/**
+	 * Getter for user count
+	 * 
+	 * @return long userCount for theme
+	 */
 	public long getUserCount() {
 		return userCount;
 	}
 
+	/**
+	 * Getter for last's comment date
+	 * 
+	 * @return {@link Date} for last's comment date
+	 */
 	public Date getLastCommentDate() {
 		return lastCommentDate;
 	}
-
+	
+	/**
+	 * String representation of this DTO object
+	 * 
+	 * @return {@link String} representation of ThemeObjectDTO
+	 */
 	@Override
 	public String toString() {
 		return String.format(
