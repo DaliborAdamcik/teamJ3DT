435bf4121a762dbd6fc884438841002c0567d527
jurda123 <jano.urda@gmail.com>
2016-08-24 13:37:52 +0200

    quickfix

--- Forum/src/sk/tsystems/forum/servlets/SignIn.java
+++ Forum/src/sk/tsystems/forum/servlets/SignIn.java
@@ -52,4 +52,11 @@ public class SignIn extends MasterServlet implements Servlet {
 		response.sendRedirect("/Forum/");
 	}
 
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request,response);
+	}
 }
